8764ca7ee239a5dc860ae00168181cb79eabc2b4
RegisSeki <regisseki@gmail.com>
2022-11-03 14:00:44 -0300

    Client CRUD

--- src/main/java/com/rtseki/algalogcourse/api/controller/ClientController.java
+++ src/main/java/com/rtseki/algalogcourse/api/controller/ClientController.java
@@ -2,22 +2,69 @@ package com.rtseki.algalogcourse.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rtseki.algalogcourse.domain.model.Client;
+import com.rtseki.algalogcourse.domain.repository.ClientRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
+
+	@Autowired
+	private ClientRepository clientRepository;
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@GetMapping
+	public List<Client> listAll() {
+		return clientRepository.findAll();
+	}
 	
-	@GetMapping("/clients")
-	public List<Client> listar() {
-		return manager.createQuery("from Client", Client.class).getResultList();
+	@GetMapping("/{clientId}")
+	public ResponseEntity<Client> findById(@PathVariable Long clientId) {
+		return clientRepository.findById(clientId)
+			.map(ResponseEntity::ok)
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client create(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+	
+	@PutMapping("/{clientId}")
+	public ResponseEntity<Client> update(@PathVariable Long clientId, @RequestBody Client client) {
+		if (!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(clientId);
+		client = clientRepository.save(client);
+		
+		return ResponseEntity.ok(client);
+	}
+	
+	@DeleteMapping("/{clientId}")
+	public ResponseEntity<Client> delete(@PathVariable Long clientId) {
+		if (!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clientRepository.deleteById(clientId);
+		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/com/rtseki/algalogcourse/domain/repository/ClientRepository.java
@@ -0,0 +1,16 @@
+package com.rtseki.algalogcourse.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rtseki.algalogcourse.domain.model.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+	
+	List<Client> findByName(String name);
+	
+	List<Client> findByNameContaining(String name);
+}
