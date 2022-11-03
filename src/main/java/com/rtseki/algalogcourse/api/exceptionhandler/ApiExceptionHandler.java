97254674151220e2d06df9ecb319833a4dbaabb4
RegisSeki <regisseki@gmail.com>
2022-11-03 16:38:48 -0300

    Add Exception Handler and custom error messages

---
+++ src/main/java/com/rtseki/algalogcourse/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.rtseki.algalogcourse.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		Problem problem = new Problem();
+		List<Problem.Field> fields = new ArrayList<>();
+		
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String name = ((FieldError) error).getField();
+			String msg = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+			
+			fields.add(new Problem.Field(name, msg));
+		}
+		
+		problem.setStatus(status.value()); 
+		problem.setDateHour(LocalDateTime.now()); 
+		problem.setTitle("One or more fields are not correct! Fill the fields correclty and try again!");
+		problem.setFields(fields);
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+	}
+}
---
+++ src/main/java/com/rtseki/algalogcourse/api/exceptionhandler/Problem.java
@@ -0,0 +1,25 @@
+package com.rtseki.algalogcourse.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Problem {
+
+	private Integer status;
+	private LocalDateTime dateHour;
+	private String title;
+	private List<Field> fields;
+	
+	@AllArgsConstructor
+	@Getter
+	public static class Field {
+		private String name;
+		private String msg;
+	}
+}
